b3923aa8b09bc5a49f9cbd465160c134f715cf68
Cristhian Manzano <crisven1998@gmail.com>
2022-01-03 15:57:04 -0500

    Se agrego el archivo readme.md y archivo de variables de entorno de ejemplo

---
+++ api/README.md
@@ -0,0 +1,1 @@
+# API - Tésis app
--- api/src/index.js
+++ api/src/index.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(morganMiddleware);
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', appRoutes);
 // Run server
