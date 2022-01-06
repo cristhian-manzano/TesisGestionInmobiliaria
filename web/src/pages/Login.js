cfd2a96051544b1175a9678c8a4320952285c2ee
Cristhian Manzano <crisven1998@gmail.com>
2022-01-06 15:42:34 -0500

    Se agrego validaciones al formulario de login

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-
 import {
   Avatar,
   Button,
@@ -11,41 +10,42 @@ import {
   Box,
   Grid,
   IconButton,
-  Typography
+  Typography,
+  FormHelperText,
+  FormControl
 } from '@mui/material';
 import { Visibility, VisibilityOff, LockOutlined } from '@mui/icons-material';
 import { Link as RouterLink } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import ImageLogin from '../assets/img/HouseLogin.jpg';
 
-const Copyright = () => {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 5 }}>
-      {'Copyright © '}
-      <MaterialLink
-        color="inherit"
-        target="_blank"
-        href="https://github.com/Cmanzano-dev/TesisGestionInmobiliaria">
-        Tésis - gestión inmobiliaria
-      </MaterialLink>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-};
+const schema = yup
+  .object({
+    email: yup.string().email().required(),
+    password: yup.string().required()
+  })
+  .required();
 
 export const Login = () => {
   const [showPassword, setShowPassoword] = useState(false);
 
-  const handleClickShowPassword = () => {
-    setShowPassoword((previous) => !previous);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit = (data) => {
+    // ready for submit
+    console.log(data);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // const data = new FormData(event.currentTarget);
-    // console.log({
-    //   email: data.get('email'),
-    //   password: data.get('password')
-    // });
+  const handleClickShowPassword = () => {
+    setShowPassoword((previous) => !previous);
   };
 
   return (
@@ -79,37 +79,47 @@ export const Login = () => {
           <Typography component="h1" variant="h5">
             Login
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Contraseña"
-              id="password"
-              autoComplete="current-password"
-              type={showPassword ? 'text' : 'password'}
-              InputProps={{
-                endAdornment: (
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    position="end">
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                )
-              }}
-            />
+          <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 1 }}>
+            <FormControl fullWidth>
+              <TextField
+                margin="normal"
+                required
+                id="email"
+                label="Email"
+                autoComplete="email"
+                autoFocus
+                {...register('email')}
+              />
+              <FormHelperText id="email" error>
+                {errors.email?.message}
+              </FormHelperText>
+            </FormControl>
+            <FormControl fullWidth>
+              <TextField
+                margin="normal"
+                required
+                label="Contraseña"
+                id="password"
+                autoComplete="current-password"
+                type={showPassword ? 'text' : 'password'}
+                {...register('password')}
+                InputProps={{
+                  endAdornment: (
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      position="end">
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  )
+                }}
+              />
+
+              <FormHelperText id="password" error>
+                {errors.password?.message}
+              </FormHelperText>
+            </FormControl>
+
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Recordarme"
@@ -117,20 +127,30 @@ export const Login = () => {
             <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
               Iniciar sesión
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <MaterialLink component={RouterLink} to="/" variant="body2">
-                  ¿Olvidó la contraseña?
-                </MaterialLink>
-              </Grid>
-              <Grid item>
-                <MaterialLink component={RouterLink} to="/register" variant="body2">
-                  Registrarse
-                </MaterialLink>
-              </Grid>
-            </Grid>
-            <Copyright />
           </Box>
+
+          <Grid container>
+            <Grid item xs>
+              <MaterialLink component={RouterLink} to="/" variant="body2">
+                ¿Olvidó la contraseña?
+              </MaterialLink>
+            </Grid>
+            <Grid item>
+              <MaterialLink component={RouterLink} to="/register" variant="body2">
+                Registrarse
+              </MaterialLink>
+            </Grid>
+          </Grid>
+          <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 5 }}>
+            {'Copyright © '}
+            <MaterialLink
+              color="inherit"
+              target="_blank"
+              href="https://github.com/Cmanzano-dev/TesisGestionInmobiliaria">
+              Tésis - gestión inmobiliaria
+            </MaterialLink>{' '}
+            {new Date().getFullYear()}.
+          </Typography>
         </Box>
       </Grid>
     </Grid>
