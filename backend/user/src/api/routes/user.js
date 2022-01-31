155ab4c80ed7b52cff62a0eb0844c20102daeca6
Cristhian Manzano <crisven1998@gmail.com>
2022-01-30 20:18:33 -0500

    error user rutas - solucionado

--- backend/user/src/api/routes/user.js
+++ backend/user/src/api/routes/user.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 
 const { getByFilter, getById, getlistTenants } = require('../controllers/user');
 
-router.get('/:id', getById);
-router.get('/tenant', getByFilter);
 router.post('/tenant/list', getlistTenants);
+router.get('/tenant', getByFilter);
+router.get('/:id', getById);
 
 module.exports = router;
