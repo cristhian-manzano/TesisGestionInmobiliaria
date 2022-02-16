4fe95f3c8cb47735780c903fb50ef5c92b9f2e24
Cristhian Manzano <crisven1998@gmail.com>
2022-02-16 16:52:12 -0500

    Correccion validaciones - contratos

--- backend/rent/src/validation/leaseAgreement.js
+++ backend/rent/src/validation/leaseAgreement.js
@@ -4,8 +4,8 @@ const leaseAgreementCreateValidation = (data) => {
   const scheme = Joi.object({
     startDate: Joi.date().required(),
     endDate: Joi.date().required(),
-    active: Joi.boolean().required(),
     idRent: Joi.number().required()
+    // active: Joi.boolean().required(),
   });
 
   return scheme.validate(data);
