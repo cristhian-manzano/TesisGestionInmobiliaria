dee849653d83b7dd8c1d0d08dc9aeff1c872e326
Cristhian Manzano <crisven1998@gmail.com>
2022-03-01 13:11:45 -0500

    correccion devolucion roles - servicio usuarios

--- backend/user/src/api/controllers/role.js
+++ backend/user/src/api/controllers/role.js
@@ -1,10 +1,18 @@
+const { Op } = require('sequelize');
+
 const Logger = require('../../config/logger');
 const Role = require('../models/role');
 const { INTERNAL_SERVER_ERROR, OK } = require('../helpers/statusCodes');
 const { errorResponse, successResponse } = require('../helpers/responsesFormat');
 
 const getAll = async (req, res) => {
-  const roles = await Role.findAll().catch((e) => {
+  const roles = await Role.findAll({
+    where: {
+      name: {
+        [Op.ne]: 'Administrador'
+      }
+    }
+  }).catch((e) => {
     Logger.error(e.toString());
   });
 
